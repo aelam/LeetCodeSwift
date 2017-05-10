7c8338d8eb0a1b8f0835ac63e778a9d66c1f757a
Ryan Wang <wanglun02@gmail.com>
2017-05-10 18:07:56 +0800

    add 17. Letter Combinations of a Phone Number

---
+++ Algorithms/17. Letter Combinations of a Phone Number/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  17. Letter Combinations of a Phone Number
+//
+//  Created by ryan on 10/05/2017.
+//  Copyright © 2017 ryan. All rights reserved.
+//
+
+class Solution {
+    static let mapping =
+        [   1: [],
+            2: ["a", "b", "c"]
+            3: ["d", "e", "f"]
+            4: ["g", "h", "i"]
+            5: ["j", "k", "l"]
+            6: ["m", "n", "o"]
+            7: ["p", "q", "r", "s"]
+            8: ["t", "u", "v"]
+            9: ["w", "x", "y", "z"]]
+    
+    func letterCombinations(_ digits: String) -> [String] {
+        
+    }
+}
