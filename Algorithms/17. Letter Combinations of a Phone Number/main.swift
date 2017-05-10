a6a68ec817e44a3673a156c1011c6407d5e9c0d7
Ryan Wang <wanglun02@gmail.com>
2017-05-10 21:20:40 +0800

    finished 17. Letter Combinations of a Phone Number

--- Algorithms/17. Letter Combinations of a Phone Number/main.swift
+++ Algorithms/17. Letter Combinations of a Phone Number/main.swift
@@ -7,18 +7,51 @@
 //
 
 class Solution {
-    static let mapping =
-        [   1: [],
-            2: ["a", "b", "c"]
-            3: ["d", "e", "f"]
-            4: ["g", "h", "i"]
-            5: ["j", "k", "l"]
-            6: ["m", "n", "o"]
-            7: ["p", "q", "r", "s"]
-            8: ["t", "u", "v"]
-            9: ["w", "x", "y", "z"]]
+    static let mapping: [[String]] =
+        [   /*0:*/ ["0"],
+            /*1:*/ ["1"],
+            /*2:*/ ["a", "b", "c"],
+            /*3:*/ ["d", "e", "f"],
+            /*4:*/ ["g", "h", "i"],
+            /*5:*/ ["j", "k", "l"],
+            /*6:*/ ["m", "n", "o"],
+            /*7:*/ ["p", "q", "r", "s"],
+            /*8:*/ ["t", "u", "v"],
+            /*9:*/ ["w", "x", "y", "z"]]
     
     func letterCombinations(_ digits: String) -> [String] {
         
+        var results = [String]()
+        for i in 0..<digits.characters.count {
+            let cc = digits[digits.index(digits.startIndex, offsetBy: i)]
+            results = combinations(results, Int(String(cc))!)
+        }
+        
+        return results
     }
+    
+    func combinations(_ results: [String], _ digit: Int) -> [String] {
+        let candidates = Solution.mapping[digit]
+        
+        if results.isEmpty {
+            return candidates
+        }
+        
+        var newResults = [String]()
+        for r in results {
+
+            for cc in candidates {
+                var rr = r
+                rr.append(cc)
+                newResults.append(rr)
+            }
+        }
+
+        return newResults
+    }
+    
 }
+
+let solution = Solution()
+print(solution.letterCombinations("2"))
+
