deea12657fc321ace1b52a1fdd07c57193c1fd92
Ryan Wang <wanglun02@gmail.com>
2017-05-11 22:48:56 +0800

    finished 11

--- Algorithms/11. Container With Most Water/main.swift
+++ Algorithms/11. Container With Most Water/main.swift
@@ -22,11 +22,46 @@ Note: You may not slant the container and n is at least 2.
     0  1  2  3  4  5  6  7  8
  
 */
-class Solution {
+class EasyUnderstandSolution {
     func maxArea(_ height: [Int]) -> Int {
-        return -1
+        var low = 0
+        var high = height.count - 1
+        var area = 0
+        
+        while low < high {
+            let minHeight = min(height[low], height[high])
+            area = max(area, minHeight * (high - low))
+            if low < high {
+                low += 1
+            } else {
+                high -= 1
+            }
+        }
+        
+        return area
     }
 }
 
-let solution = Solution()
-print(solution.maxArea([1,3,4,5,6,7,2,3]))
+class Solution {
+    func maxArea(_ height: [Int]) -> Int {
+        var low = 0
+        var high = height.count - 1
+        var area = 0
+        
+        while low < high {
+            let minHeight = min(height[low], height[high])
+            area = max(area, minHeight * (high - low))
+            while low < high && height[low] <= minHeight {
+                low += 1
+            }
+            while low < high && height[high] <= minHeight {
+                high -= 1
+            }
+        }
+
+        return area
+    }
+}
+
+let solution = EasyUnderstandSolution()
+print(solution.maxArea([1,2,4,3]))
