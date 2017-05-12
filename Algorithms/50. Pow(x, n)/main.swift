486ece42221d508acd93c2ecbad94f220974d5dc
Ryan Wang <wanglun02@gmail.com>
2017-05-12 23:51:34 +0800

    finished 50. pow

--- Algorithms/50. Pow(x, n)/main.swift
+++ Algorithms/50. Pow(x, n)/main.swift
@@ -6,8 +6,25 @@
 //  Copyright © 2017 ryan. All rights reserved.
 //
 
+// Implement pow(x, n).
+
+
+// 时间复杂度O(log(n))
+
 class Solution {
     func myPow(_ x: Double, _ n: Int) -> Double {
+        let nn = abs(n)
+        var xx = x
+        if n == 0 {
+            return 1
+        } else if n < 0 {
+            xx = 1/x
+        }
         
+        return nn % 2 == 0 ? myPow(xx * xx, nn / 2) : xx * myPow(xx * xx, nn / 2)
     }
 }
+
+
+let solution = Solution()
+print(solution.myPow(2, -5))
